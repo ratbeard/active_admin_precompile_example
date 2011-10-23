73dc69e3baef22bf09374229ce916e1e4f8ce044
Ben Atkin <ben@benatkin.com>
2011-10-22 22:23:45 -0600

    rails new; add active_admin and metasearch to Gemfile; bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActiveAdminPrecompileExample::Application.config.secret_token = 'bf0c35d502bc37c44eb43b945045d95e1a807fe4b414ceb3779f6da5c8c4dd60bc5182ea9aca69b5884c335761d48bdc33a293e1b41538fcfe89e8aa0fa94701'
